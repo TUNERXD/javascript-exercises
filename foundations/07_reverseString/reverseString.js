431e441b6f5e8b6caed936b221ff6cf4560ab279
Tharathep Laohaviroj <tuner.tharathep@gmail.com>
2026-06-19 15:41:23 +0700

    feat: completed reverseString.js

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let len = str.length;
+    let result = ""
+    for (let i = 0; i < len; i++) {
+        result += str[len - (i + 1)];
+    }
+    return result;
 };
 
 // Do not edit below this line
