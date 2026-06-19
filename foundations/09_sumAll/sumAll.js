2f16e672e2cfdb90518bb8ea362729207ad15de6
Tharathep Laohaviroj <tuner.tharathep@gmail.com>
2026-06-19 16:41:31 +0700

    feat: finished sumAll.js

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0){
+        return "ERROR";
+    }
+    if (start == end) {
+        return start;
+    }
+    if (end < start) {
+        [start, end] = [end, start]
+    }
+    let result = 0;
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
